576651efcd499e7ef09fc8e618033b0d9ce2016f
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2025-06-10 13:26:55 -0400

    Add notes to form

--- components/KeyForm.js
+++ components/KeyForm.js
@@ -1,4 +1,5 @@
-import { View, Text, Picker } from 'react-native';
+import { View, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import { useState } from 'react';
 
 function KeyForm() {
@@ -8,10 +9,20 @@ function KeyForm() {
     <View>
         <Text>Using the shapes of:</Text>
         <Picker 
-        id="transposed"
         selectedValue={selectedValue}
         onValueChange={(itemValue) => setSelectedValue(itemValue)}>
             <Picker.Item label="A" value={1}/>
+            <Picker.Item label="B♭" value={2}/>
+            <Picker.Item label="B" value={3}/>
+            <Picker.Item label="C" value={4}/>
+            <Picker.Item label="D♭" value={5}/>
+            <Picker.Item label="D" value={6}/>
+            <Picker.Item label="E♭" value={7}/>
+            <Picker.Item label="E" value={8}/>
+            <Picker.Item label="F" value={9}/>
+            <Picker.Item label="G♭" value={10}/>
+            <Picker.Item label="G" value={11}/>
+            <Picker.Item label="A♭" value={12}/>
         </Picker>
     </View>
     );
